67a02700c68c67f4d25c5dda40ae53deeb2b62d0
Rickard Bruel Gabrielsson <rgabriel@stanford.edu>
2021-11-15 11:59:21 -0500

    fix

--- examples/stl10/main.py
+++ examples/stl10/main.py
@@ -110,7 +110,7 @@ def main():
         # MST inds
         def mst_loss(y, t=2.0):
             #pdist = torch.cdist(y, y, p=t)
-            pdist = torch.pdist(x, p=2).pow(2).mul(-t).exp() # .mean().log()
+            pdist = torch.pdist(y, y, p=2).pow(2).mul(-t).exp() # .mean().log()
             #pdist = pdist + torch.diag(torch.tensor([1e10] * len(pdist))).to(y.device)
             rows, cols = cpp_mst(pdist) # get_mst_indices(pdist)
             loss = torch.mean(pdist[rows.to(y.device), cols.to(y.device)]).log()
